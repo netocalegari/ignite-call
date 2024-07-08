c81a474caf9484f880b9f2b8c9513bed3f4ac06c
netocalegari <netokalegari@gmail.com>
2024-07-08 11:57:08 -0300

    permissions

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,14 +1,20 @@
 import { signIn, useSession } from "next-auth/react";
-import { ArrowRight } from "phosphor-react";
+import { useRouter } from "next/router";
+import { ArrowRight, Check } from "phosphor-react";
 
 import { api } from "@/lib/axios";
 import { Button, Heading, MultiStep, Text } from "@ignite-ui/react";
 
 import { Container, Header } from "../styles";
-import { ConnectBox, ConnectItem } from "./styles";
+import { AuthError, ConnectBox, ConnectItem } from "./styles";
 
 export default function Register() {
-  const session = useSession({});
+  const session = useSession();
+
+  const router = useRouter();
+
+  const hasAuthError = !!router.query.error;
+  const isSignedIn = session.status === "authenticated";
   // const handleRegister = async (data) => {};
 
   return (
@@ -26,17 +32,31 @@ export default function Register() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button
-            variant="secondary"
-            size="sm"
-            onClick={() => signIn("google")}
-          >
-            Connect
-            <ArrowRight />
-          </Button>
+          {isSignedIn ? (
+            <Button
+              variant="secondary"
+              size="sm"
+              onClick={() => signIn("google")}
+            >
+              Connect
+              <ArrowRight />
+            </Button>
+          ) : (
+            <Button>
+              Connected
+              <Check />
+            </Button>
+          )}
         </ConnectItem>
 
-        <Button type="submit">
+        {hasAuthError && (
+          <AuthError size="sm">
+            Error connecting to Google. Verify if you have accepted all
+            necessary access permissions.
+          </AuthError>
+        )}
+
+        <Button type="submit" disabled={isSignedIn}>
           Next step
           <ArrowRight />
         </Button>
