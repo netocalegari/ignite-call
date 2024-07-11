c7b88bf920dbf3bff498950ab090c663ec0f27fd
netocalegari <netokalegari@gmail.com>
2024-07-11 08:21:16 -0300

    generating days of the week

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -1,5 +1,8 @@
 import { ArrowRight } from "phosphor-react";
+import { useFieldArray, useForm } from "react-hook-form";
+import { z } from "zod";
 
+import { getWeekDays } from "@/utils/get-week-days";
 import {
   Button,
   Checkbox,
@@ -18,7 +21,72 @@ import {
   IntervalsContainer,
 } from "./styles";
 
+const timeIntervalsFormSchema = z.object({});
+
 export default function TimeIntervals() {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { isSubmitting, errors },
+  } = useForm({
+    defaultValues: {
+      intervals: [
+        {
+          weekDay: 0,
+          enabled: false,
+          startTime: "08:00",
+          endTime: "18:00",
+        },
+        {
+          weekDay: 1,
+          enabled: true,
+          startTime: "08:00",
+          endTime: "18:00",
+        },
+        {
+          weekDay: 2,
+          enabled: true,
+          startTime: "08:00",
+          endTime: "18:00",
+        },
+        {
+          weekDay: 3,
+          enabled: true,
+          startTime: "08:00",
+          endTime: "18:00",
+        },
+        {
+          weekDay: 4,
+          enabled: true,
+          startTime: "08:00",
+          endTime: "18:00",
+        },
+        {
+          weekDay: 5,
+          enabled: true,
+          startTime: "08:00",
+          endTime: "18:00",
+        },
+        {
+          weekDay: 6,
+          enabled: false,
+          startTime: "08:00",
+          endTime: "18:00",
+        },
+      ],
+    },
+  });
+
+  const weekDays = getWeekDays();
+
+  const { fields } = useFieldArray({
+    name: "intervals",
+    control,
+  });
+
+  const handleSetTimeIntervals = async () => {};
+
   return (
     <Container>
       <Header>
@@ -28,29 +96,30 @@ export default function TimeIntervals() {
         <MultiStep size={4} currentStep={2} />
       </Header>
 
-      <IntervalBox as="form">
+      <IntervalBox as="form" onSubmit={handleSubmit(handleSetTimeIntervals)}>
         <IntervalsContainer>
-          <IntervalItem>
-            <IntervalDay>
-              <Checkbox />
-              <Text>Monday</Text>
-            </IntervalDay>
-            <IntervalInputs>
-              <TextInput size="sm" type="time" step={60} />
-              <TextInput size="sm" type="time" step={60} />
-            </IntervalInputs>
-          </IntervalItem>
-
-          <IntervalItem>
-            <IntervalDay>
-              <Checkbox />
-              <Text>Tuesday</Text>
-            </IntervalDay>
-            <IntervalInputs>
-              <TextInput size="sm" type="time" step={60} />
-              <TextInput size="sm" type="time" step={60} />
-            </IntervalInputs>
-          </IntervalItem>
+          {fields.map((field, index) => (
+            <IntervalItem key={field.id}>
+              <IntervalDay>
+                <Checkbox />
+                <Text>{weekDays[field.weekDay]}</Text>
+              </IntervalDay>
+              <IntervalInputs>
+                <TextInput
+                  size="sm"
+                  type="time"
+                  step={60}
+                  {...register(`intervals.${index}.startTime`)}
+                />
+                <TextInput
+                  size="sm"
+                  type="time"
+                  step={60}
+                  {...register(`intervals.${index}.endTime`)}
+                />
+              </IntervalInputs>
+            </IntervalItem>
+          ))}
         </IntervalsContainer>
 
         <Button type="submit">
